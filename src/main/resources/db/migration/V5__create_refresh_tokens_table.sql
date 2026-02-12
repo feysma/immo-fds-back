30fc2f9ea591ec4399ace7a1ba07187f29f8baeb
Speareat <speareatgtpsn@gmail.com>
2026-02-12 09:46:36 +0100

    :recycle: upgrade from flyway to liquibase

--- src/main/resources/db/migration/V5__create_refresh_tokens_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE refresh_tokens (
-    id          BIGSERIAL PRIMARY KEY,
-    token       VARCHAR(255) NOT NULL UNIQUE,
-    user_id     BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    expiry_date TIMESTAMP WITH TIME ZONE NOT NULL
-);
-
-CREATE INDEX idx_refresh_tokens_token ON refresh_tokens (token);
-CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens (user_id);
